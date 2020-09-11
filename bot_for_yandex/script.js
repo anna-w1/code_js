2fe2a1311f947a012a5bb13836a5be3b9a2934e1
anna-w1 <71127794+anna-w1@users.noreply.github.com>
2020-09-12 01:45:23 +0300

    Create script.js

---
+++ bot_for_yandex/script.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         Bot for yandex
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://yandex.ru/*
+// @grant        none
+// ==/UserScript==
+
+document.getElementsByName('text')[0].value = "Гобой"
+
+document.getElementsByClassName('button mini-suggest__button button_theme_websearch button_size_ws-head i-bem button_js_inited')[0];
+
+let keywords = ['Гобой','Как звучит флейта', 'Кларнет'];
+let keyword = keywords[getRandom(0,keywords.length)];
+let search = document.getElementsByClassName('button mini-suggest__button button_theme_websearch button_size_ws-head i-bem button_js_inited')[0];
+
+if (search != undefined){
+    document.getElementsByName('text')[0].value = keyword;
+    document.getElementsByClassName('button mini-suggest__button button_theme_websearch button_size_ws-head i-bem button_js_inited')[0].click();
+}else{
+    let links = document.links;
+
+    for(let i=0; i<links.length; i++){
+        if(links[i].href.indexOf("muz-story.ru") != -1){
+            console.log("Ссылка найдена "+links[i]);
+            links[i].click();
+            break;
+        }
+    }
+}
+
+function getRandom(min,max){
+    return Math.floor(Math.random()*(max-min)+min);
+}
